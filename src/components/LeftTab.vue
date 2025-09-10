9dbcae5ec2c29e6dd9ccb73fde439df753bec375
LiveisFpv <nasonovnicolai@gmail.com>
2025-09-10 23:56:34 +0300

    Start project

---
+++ src/components/LeftTab.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="left-tab">
+    <div class="header">
+      <button class="btn">Logo</button>
+      <button class="btn">&lt;-]</button>
+    </div>
+  </div>
+</template>
+<style lang="css" scoped>
+.left-tab {
+  width: 250px;
+  height: 100vh;
+  background-color: var(--color-bg-secondary);
+  border-right: 1px solid var(--color-border);
+  border-bottom-right-radius: 25px;
+  border-top-right-radius: 25px;
+  padding: 20px 0px;
+}
+.header {
+  padding: 0px 20px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  gap: 20px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LeftTab from '@/components/LeftTab.vue'
+</script>
 <template>
   <div>
+    <LeftTab />
     <h1>Home View</h1>
   </div>
 </template>
